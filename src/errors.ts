7cd4f8af3e7ce9b81db4a8c38390b783e0153f42
devnote-dev <pokeraven4@gmail.com>
2022-01-08 20:27:35 +0000

    chore: add missing error messages

--- src/errors.ts
+++ src/errors.ts
@@ -1,5 +1,7 @@
 export const ERRORS = {
-    MISSING_ENV: "Environment variable 'SOAR_PATH' is not set. Please set this to the path of the Soar library."
+    MISSING_ENV: "Environment variable 'SOAR_PATH' is not set. Please set this to the path of the Soar library.",
+    INVALID_ENV: "Environment variable 'SOAR_PATH' path does not exist.",
+    CANNOT_READ_ENV: 'Soar config file could not be opened. Please make sure Soar has the necessary permissions.'
 }
 
 export class SoarError extends Error {}
