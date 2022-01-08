e063dcd711749d98d48b2fa0cc15996fb7813d33
devnote-dev <pokeraven4@gmail.com>
2022-01-08 21:33:56 +0000

    feat(app): add command help text

--- src/app/index.ts
+++ src/app/index.ts
@@ -3,6 +3,7 @@ import { handleRequest } from '../request';
 import { parseStruct, AppUser } from '../structs';
 
 const getUsersCmd = new Command('get-users')
+    .addHelpText('before', 'Fetches all accounts from the panel (can specify or query with flags).')
     .action(async () => {
         const data = await handleRequest('GET', '/api/application/users');
         const user = parseStruct<AppUser>(data);
