ded604afbe03c383b5f5a46cf4b56bef54528cbd
devnote-dev <pokeraven4@gmail.com>
2022-01-13 23:56:26 +0000

    refactor(log): rewrite log func implementations

--- src/log/index.ts
+++ src/log/index.ts
@@ -11,38 +11,75 @@ export const COLOURS = {
     MAGENTA: '\x1b[35m'
 }
 
+export const CODE_MAP = {
+    '%R': COLOURS.RESET,
+    '%r': COLOURS.RED,
+    '%y': COLOURS.YELLOW,
+    '%g': COLOURS.GREEN,
+    '%c': COLOURS.CYAN,
+    '%b': COLOURS.BLUE,
+    '%m': COLOURS.MAGENTA
+}
+
 const BASE = `[${COLOURS.CYAN}soar${COLOURS.RESET}]`;
+
+export function parse(
+        message: string,
+        type?: 'info' | 'success' | 'notice' | 'warn' | 'error'
+    ): string {
+    for (const [k, c] of Object.entries<string>(CODE_MAP)) message = message.replaceAll(k, c);
+
+    if (type) {
+        switch (type) {
+            case 'info': message = `${BASE} ${COLOURS.BLUE}info${COLOURS.RESET}: ${message}`; break;
+            case 'success': message = `${BASE} ${COLOURS.GREEN}success${COLOURS.RESET}: ${message}`; break;
+            case 'notice': message = `${BASE} ${COLOURS.BLUE}notice${COLOURS.RESET}: ${message}`; break;
+            case 'warn': message = `${BASE} ${COLOURS.YELLOW}warning${COLOURS.RESET}: ${message}`; break;
+            case 'error': message = `${BASE} ${COLOURS.RED}error${COLOURS.RESET}: ${message}`; break;
+        }
+    }
+
+    return message;
+}
+
+export function print(message: string): void {
+    console.log(parse(message));
+}
 
 export function info(message: string): void {
     console.log(`${BASE} ${COLOURS.BLUE}info${COLOURS.RESET}: ${message}`);
 }
 
-export function warn(type: string, message: string[]): void {
-    const border = `${BASE} ${COLOURS.YELLOW}warning${COLOURS.RESET}: `;
-    let fmt = border + type +'\n';
-    fmt += message.map(m => border + m).join('\n');
-    console.log(fmt);
+export function success(message: string): void {
+    console.log(`${BASE} ${COLOURS.GREEN}success${COLOURS.RESET}: ${message}`);
 }
 
-export function error(type: string, message?: string | string[], exit?: boolean): void | never {
-    const border = `${BASE} ${COLOURS.RED}error${COLOURS.RESET}: `;
-    let fmt = border +'\n';
+export function warn(message: string | string[]): void {
+    const border = parse(`${BASE} %ywarning%R: `);
+    const fmt = Array.isArray(message) ? message : [message];
+
+    console.log(fmt.map(m => border + m).join('\n'));
+}
+
+export function error(name: string, message?: string | string[], exit?: boolean): void | never {
+    const border = parse(`${BASE} %rerror%R: `);
+    let fmt: string[] = [name];
 
     if (!message) {
-        if (errors.tryGet(type)) {
-            fmt += errors.get(type);
+        if (errors.tryGet(name)) {
+            fmt.push(errors.get(name));
         } else {
-            fmt += 'INVALID LOG MESSSAGE';
+            fmt.push('INVALID LOG MESSAGE');
         }
     } else {
         if (Array.isArray(message)) {
-            fmt += message.map(m => border + m).join('\n');
+            fmt = fmt.concat(message);
         } else {
-            fmt += border + message;
+            fmt.push(message);
         }
     }
 
-    console.log(fmt);
+    console.log(fmt.map(m => border + m).join('\n'));
     if (exit) process.exit(1);
 }
 
