4cb81fd52aa39cad31c1437021b1234f289a877b
devnote-dev <pokeraven4@gmail.com>
2022-01-13 23:57:00 +0000

    feat(app): implement get-users updated

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,14 +1,19 @@
 import { Command } from 'commander';
 import { handleRequest } from '../request';
-import { parseStruct, AppUser } from '../structs';
+import * as res from '../response';
+import { parseUserGroup } from '../validate';
 
 const getUsersCmd = new Command('get-users')
     .addHelpText('before', 'Fetches all accounts from the panel (can specify or query with flags).')
-    .action(async () => {
+    .option('--json', 'Send the response output as JSON.', true)
+    .option('--yaml', 'Send the response output as YAML.', false)
+    .option('-n, --no-prompt', 'Don\'t prompt for user response after the request.', false)
+    .option('-s, --silent', 'Don\'t log request messages.', false)
+    .action(async (args: object) => {
+        const options = parseUserGroup(args);
         const data = await handleRequest('GET', '/api/application/users');
-        const user = parseStruct<AppUser>(data);
-
-        console.log(user);
+        const out = res.handleCloseInterface(data, options);
+        if (out) console.log(out);
     });
 
 const main = new Command('app')
