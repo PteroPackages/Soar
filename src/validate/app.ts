70a08bc12f0002393a7227ee9afcc3122abb59ac
devnote-dev <pokeraven4@gmail.com>
2022-01-14 22:43:55 +0000

    feat(validate): update user flags parsing

--- src/validate/app.ts
+++ src/validate/app.ts
@@ -1,9 +1,18 @@
 import { FlagOptions } from '../structs';
 
 export function parseUserGroup(args: object): FlagOptions {
+    const type = (args['text'] && 'text') || (args['yaml'] && 'yaml') || 'json';
+    let file = '';
+
+    if (args['output']) {
+        if (typeof args['output'] === 'boolean') file = `soar_log_${Date.now()}`;
+        else file = args['output'];
+    }
+    if (!file.endsWith('.'+ type)) file += '.'+ type;
+
     return {
-        prompt: !!args['silent'],
-        writeFile: (args['str'] && 'str') || (args['yaml'] && 'yaml') || 'json',
-        responseType: args['output']
+        prompt: args['silent'],
+        writeFile: file,
+        responseType: type
     } as FlagOptions;
 }
