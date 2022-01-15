60a9f4f9bae6b9b6e0656551ec156936c0b17bea
devnote-dev <pokeraven4@gmail.com>
2022-01-15 02:54:41 +0000

    feat(app, response): merge writer, implement text response

--- src/response/index.ts
+++ src/response/index.ts
@@ -1,5 +1,7 @@
 import { createInterface } from 'readline';
-import * as writer from './writer';
+import { writeFileSync } from 'fs';
+import { join } from 'path';
+import yaml from 'yaml';
 import { FlagOptions } from '../structs';
 
 export function getBoolInput(message: string): boolean {
@@ -35,22 +37,75 @@ export function getOptionInput(
     return res;
 }
 
+function getMaxLength(data: object): number {
+    let max = 0;
+
+    for (const [k, v] of Object.entries(data)) {
+        if (k.length > max) max = k.length;
+        if (
+            typeof v === 'object' &&
+            !Array.isArray(v) &&
+            v !== null
+        ) {
+            const t = getMaxLength(v);
+            if (t > max) max = t;
+        }
+    }
+
+    return max;
+}
+
+function formatString(data: object): string {
+    const max = getMaxLength(data);
+    let res: string[] = [];
+
+    if (Array.isArray(data)) return data.map(formatString).join('\n');
+
+    for (let [key, val] of Object.entries(data)) {
+        let fmt = `${key}: ${' '.repeat(max - key.length)}`;
+        if (
+            typeof val === 'object' &&
+            val !== null
+        ) {
+            fmt += '<object ref>\n';
+            fmt += formatString(val);
+        } else {
+            fmt += `${val}`;
+        }
+        res.push(fmt);
+    }
+
+    return res.join('\n');
+}
+
+function writeFileResponse(name: string, res: string) {
+    try {
+        writeFileSync(
+            join(process.cwd(), name),
+            Buffer.from(res),
+            { encoding: 'utf-8' }
+        );
+    } catch (err) {
+        console.error(err);
+    }
+}
+
 export function handleCloseInterface(data: object, options: FlagOptions): string | void {
     let parsed: string;
 
     switch (options.responseType) {
-        case 'text': parsed = writer.formatString(data); break;
-        case 'yaml': parsed = writer.formatYAML(data); break;
+        case 'text': parsed = formatString(data); break;
+        case 'yaml': parsed = yaml.stringify(data); break;
         default: parsed = JSON.stringify(data); break;
     }
 
     if (options.writeFile.length) {
-        writer.writeFileResponse(options.writeFile, parsed);
+        writeFileResponse(options.writeFile, parsed);
     } else if (options.prompt && !options.silent) {
         const res = getBoolInput('Should this request be saved? (y/n)');
         if (res) {
             const fp = getStringInput('Enter the file path to save to: ', true);
-            if (fp) writer.writeFileResponse(`soar_log_${Date.now()}`, parsed);
+            if (fp) writeFileResponse(`soar_log_${Date.now()}`, parsed);
         }
     }
 
