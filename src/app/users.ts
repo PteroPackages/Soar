adc0b517f0769c406490dffd0b0569f3da80b861
devnote-dev <pokeraven4@gmail.com>
2022-01-28 18:11:17 +0000

    feat(app): implement update-users command

--- src/app/users.ts
+++ src/app/users.ts
@@ -1,6 +1,7 @@
 import { Command } from 'commander';
 import { handleRequest } from '../request';
 import * as res from '../response';
+import parseDiffView, { highlight } from '../response/view';
 import { buildUser, parseUserGroup } from '../validate';
 import * as log from '../log';
 import Waiter from '../log/waiter';
@@ -38,6 +39,55 @@ const getUsersCmd = new Command('get-users')
         if (out) console.log(out);
     });
 
+const updateUsersCmd = new Command('update-users')
+    .addHelpText('before', 'Updates a specified user account.')
+    .argument('<id>', 'The ID of the user to update.')
+    .option('-n, --no-prompt', 'Don\'t prompt for user response after the request.', false)
+    .option('-s, --silent', 'Don\'t log request messages.', false)
+    .option('-o, --output [file]', 'Writes the output to a file.')
+    .option('-d, --data <json>', 'The json data to update the user with.')
+    .option('-c, --changes', 'Shows the properties changed in the request.', false)
+    .action(async (id: string, args: object) => {
+        const options = parseUserGroup(args);
+        let waiter: Waiter;
+
+        if (!options.silent) {
+            waiter = new Waiter(log.parse('%yfetching%R /application/users', 'info'))
+                .onEnd(t => log.parse(`%gfetched%R /application/users (${t}ms taken)`, 'info'))
+                .start();
+        }
+
+        let json: object;
+        try {
+            json = JSON.parse(args['data']);
+        } catch (err) {
+            log.fromError(err, true);
+        }
+
+        const user = await handleRequest('GET', buildUser({ id }));
+        if (!user) log.error('NOT_FOUND_USER', null, true);
+
+        json['username'] ||= user['attributes']['username'];
+        json['email'] ||= user['attributes']['email'];
+        json['first_name'] ||= user['attributes']['first_name'];
+        json['last_name'] ||= user['attributes']['last_name'];
+        json['language'] || user['attributes']['language'];
+        json['password'] ||= null;
+
+        const data = await handleRequest('PATCH', buildUser({ id }), json);
+        if (!options.silent) waiter.stop();
+
+        const out = res.handleCloseInterface(data, options);
+        if (out) {
+            const view = parseDiffView('yaml', user, data);
+            log.info(log.parse(
+                `made %c${view.totalChanges}%R changes (%g+${view.additions}%R | %r-${view.subtractions}%R)`
+            ));
+            console.log('\n'+ highlight(view.output));
+        }
+    });
+
 export default [
-    getUsersCmd
+    getUsersCmd,
+    updateUsersCmd
 ]
