6735ff45797ce0257c99854330debe497e9e1ec8
devnote-dev <pokeraven4@gmail.com>
2022-01-30 19:04:11 +0000

    fix(app): fix weird success-info log conflict

--- src/app/users.ts
+++ src/app/users.ts
@@ -126,9 +126,8 @@ const updateUserCmd = new Command('update-user')
         const out = res.handleCloseInterface(data, options);
         if (out && args['diff']) {
             const view = parseDiffView(options.responseType, user, data);
-            log.info(log.parse(
-                `made %c${view.totalChanges}%R changes (%g+${view.additions}%R | %r-${view.subtractions}%R)`,
-                'success'
+            log.success(log.parse(
+                `made %c${view.totalChanges}%R changes (%g+${view.additions}%R | %r-${view.subtractions}%R)`
             ));
             console.log('\n'+ highlight(view.output));
         } else {
