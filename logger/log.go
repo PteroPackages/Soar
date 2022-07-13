846efc24d32593096bccaca8112c97f4cbdf73b6
devnote-dev <inferno0745@gmail.com>
2022-07-13 22:47:44 +0100

    fix(logger): fix color codes, add blank map

--- logger/log.go
+++ logger/log.go
@@ -32,15 +32,16 @@ func (l *Logger) SetLevel(level int) *Logger {
 	return l
 }
 
-var colorMap = strings.NewReplacer("%R", "\x1b[31m", "%Y", "\x1b[33m", "%B", "\x1b[34m", "%Z", "\x1b[0m")
+var colorMap = strings.NewReplacer("$R", "\x1b[31m", "$Y", "\x1b[33m", "$B", "\x1b[34m", "$Z", "\x1b[0m")
+var blankMap = strings.NewReplacer("$R", "\x1b[0m", "$Y", "\x1b[0m", "$B", "\x1b[0m", "$Z", "\x1b[0m")
 
 func (l *Logger) color(data string, args ...interface{}) string {
 	str := fmt.Sprintf(data, args...)
 	if l.UseColor {
 		return colorMap.Replace(str)
+	} else {
+		return blankMap.Replace(str)
 	}
-
-	return str
 }
 
 func (l *Logger) Debug(data string, args ...interface{}) {
@@ -54,21 +55,24 @@ func (l *Logger) Line(data string, args ...interface{}) *Logger {
 	return l
 }
 
-func (l *Logger) WithCmd(data string) *Logger {
-	l.writer.WriteString(l.color("run '%s' for more information", data))
+func (l *Logger) WithCmd(cmd string) *Logger {
+	l.writer.WriteString(l.color("run '%s' for more information", cmd))
 	return l
 }
 
 func (l *Logger) Info(data string, args ...interface{}) {
-	l.writer.WriteString(l.color("%Binfo%Z: "+data, args))
+	l.writer.WriteString(l.color("$Binfo$Z: "))
+	l.writer.WriteString(fmt.Sprintf(data, args...) + "\n")
 }
 
 func (l *Logger) Warn(data string, args ...interface{}) {
-	l.writer.WriteString(l.color("%Ywarn%Z: "+data, args))
+	l.writer.WriteString(l.color("$Ywarn$Z: "))
+	l.writer.WriteString(fmt.Sprintf(data, args...) + "\n")
 }
 
 func (l *Logger) Error(data string, args ...interface{}) *Logger {
-	os.Stderr.WriteString(l.color("%Rerror%Z: "+data, args))
+	os.Stderr.WriteString(l.color("$Rerror$Z: "))
+	os.Stderr.WriteString(fmt.Sprintf(data, args...) + "\n")
 	return l
 }
 
