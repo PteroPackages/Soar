945bc06e00bbe42b3d1a0efdd7e8c571961a4960
devnote-dev <devnote.dev75@gmail.com>
2023-12-01 23:42:11 +0000

    feat(commands): add basic base structure

---
+++ src/commands/base.cr
@@ -0,0 +1,23 @@
+module Soar::Commands
+  abstract class Base < Cling::Command
+    def initialize
+      super
+
+      @inherit_options = true
+      add_option "no-color"
+      add_option 'h', "help"
+    end
+
+    def pre_run(arguments : Cling::Arguments, optionns : Cling::Options) : Bool
+      Colorize.enabled = false if options.has? "no-color"
+
+      if options.has? "help"
+        stdout.puts help_template
+
+        false
+      else
+        true
+      end
+    end
+  end
+end
