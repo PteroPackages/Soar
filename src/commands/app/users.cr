7de9dce728b33c597c4dcf6cbb5b622cf1c69ad3
devnote-dev <devnote.dev75@gmail.com>
2023-12-27 12:44:31 +0000

    refactor(app): redesign users list format

--- src/commands/app/users.cr
+++ src/commands/app/users.cr
@@ -17,19 +17,48 @@ module Soar::Commands::App
 
       def run(arguments : Cling::Arguments, options : Cling::Options) : Nil
         users = request get: "/api/application/users", as: Array(Models::User)
-
-        stdout << users.size << " Result" << (users.size == 1 ? '\n' : "s\n")
         return if users.empty?
-        stdout.puts
+
+        width = 2 + (Math.log(users.last.id.to_f + 1) / Math.log(10)).ceil.to_i
 
         users.each do |user|
-          stdout << ' ' << user.id.colorize.bold.on_light_gray << ' ' << user.username
-          if external = user.external_id
-            stdout << " (" << external << ")"
+          Colorize.with.bold.on_light_gray.surround(stdout) do |io|
+            io << user.id.to_s.center(width)
           end
 
-          stdout << "\n " << user.first_name << ' ' << user.last_name
-          stdout << " <" << user.email << ">\n\n"
+          if external = user.external_id
+            stdout << ' '
+            Colorize.with.dark_gray.surround(stdout) do |io|
+              io << '[' << external << ']'
+            end
+          end
+
+          stdout << "\n\n┃ ".colorize.light_gray << "username:  ".colorize.bold
+          stdout << user.username
+          stdout << "\n┃ ".colorize.light_gray << "full name: ".colorize.bold
+          stdout << user.first_name << ' ' << user.last_name
+
+          stdout << "\n┃ ".colorize.light_gray << "email:     ".colorize.bold << user.email
+          stdout << "\n┃ ".colorize.light_gray << "language:  ".colorize.bold << user.language
+
+          stdout << "\n┃ ".colorize.light_gray << "is admin:  ".colorize.bold
+          stdout << (user.root_admin? ? "true".colorize.green : "false".colorize.red)
+
+          stdout << "\n┃ ".colorize.light_gray << "has 2FA:   ".colorize.bold
+          stdout << (user.two_factor? ? "true".colorize.green : "false".colorize.red)
+
+          stdout << "\n┃ ".colorize.light_gray << "created:   ".colorize.bold
+          user.created_at.to_s(stdout, "%F %R")
+
+          # TODO: implement 'x hours y minutes ago' format for updated at
+          stdout << "\n┃ ".colorize.light_gray << "updated:   ".colorize.bold
+          if updated = user.updated_at
+            updated.to_s(stdout, "%F %R")
+          else
+            stdout << "N/A".colorize.dark_gray
+          end
+
+          stdout << "\n\n"
         end
       end
     end
