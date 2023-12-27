e9d7396606b84761e4a4d4df39bee8c38e821389
devnote-dev <devnote.dev75@gmail.com>
2023-12-27 21:49:33 +0000

    refactor(app, models): move user formatting into model

--- src/commands/app/users.cr
+++ src/commands/app/users.cr
@@ -76,44 +76,8 @@ module Soar::Commands::App
         end
 
         width = 2 + (Math.log(users.last.id.to_f + 1) / Math.log(10)).ceil.to_i
-
         users.each do |user|
-          Colorize.with.bold.on_light_gray.surround(stdout) do |io|
-            io << user.id.to_s.center(width)
-          end
-
-          if external = user.external_id
-            stdout << ' '
-            Colorize.with.dark_gray.surround(stdout) do |io|
-              io << '[' << external << ']'
-            end
-          end
-
-          stdout << "\n\n┃ ".colorize.light_gray << "username:  ".colorize.bold
-          stdout << user.username
-          stdout << "\n┃ ".colorize.light_gray << "full name: ".colorize.bold
-          stdout << user.first_name << ' ' << user.last_name
-
-          stdout << "\n┃ ".colorize.light_gray << "email:     ".colorize.bold << user.email
-          stdout << "\n┃ ".colorize.light_gray << "language:  ".colorize.bold << user.language
-
-          stdout << "\n┃ ".colorize.light_gray << "is admin:  ".colorize.bold
-          stdout << (user.root_admin? ? "true".colorize.green : "false".colorize.red)
-
-          stdout << "\n┃ ".colorize.light_gray << "has 2FA:   ".colorize.bold
-          stdout << (user.two_factor? ? "true".colorize.green : "false".colorize.red)
-
-          stdout << "\n┃ ".colorize.light_gray << "created:   ".colorize.bold
-          user.created_at.to_s(stdout, "%F %R")
-
-          # TODO: implement 'x hours y minutes ago' format for updated at
-          stdout << "\n┃ ".colorize.light_gray << "updated:   ".colorize.bold
-          if updated = user.updated_at
-            updated.to_s(stdout, "%F %R")
-          else
-            stdout << "N/A".colorize.dark_gray
-          end
-
+          user.to_s(stdout, width)
           stdout << "\n\n"
         end
 
@@ -123,7 +87,7 @@ module Soar::Commands::App
         stdout << "\n┃ ".colorize.light_gray
 
         if @filters.empty?
-          stdout << "no filters applied"
+          stdout << "no filters applied".colorize.dark_gray
         else
           stdout << "filters: ".colorize.dark_gray << @filters.join(", ")
         end
@@ -154,40 +118,7 @@ module Soar::Commands::App
         end
 
         width = 2 + (Math.log(user.id.to_f + 1) / Math.log(10)).ceil.to_i
-        Colorize.with.bold.on_light_gray.surround(stdout) do |io|
-          io << user.id.to_s.center(width)
-        end
-
-        if external = user.external_id
-          stdout << ' '
-          Colorize.with.dark_gray.surround(stdout) do |io|
-            io << '[' << external << ']'
-          end
-        end
-
-        stdout << "\n\n┃ ".colorize.light_gray << "username:  ".colorize.bold
-        stdout << user.username
-        stdout << "\n┃ ".colorize.light_gray << "full name: ".colorize.bold
-        stdout << user.first_name << ' ' << user.last_name
-
-        stdout << "\n┃ ".colorize.light_gray << "email:     ".colorize.bold << user.email
-        stdout << "\n┃ ".colorize.light_gray << "language:  ".colorize.bold << user.language
-
-        stdout << "\n┃ ".colorize.light_gray << "is admin:  ".colorize.bold
-        stdout << (user.root_admin? ? "true".colorize.green : "false".colorize.red)
-
-        stdout << "\n┃ ".colorize.light_gray << "has 2FA:   ".colorize.bold
-        stdout << (user.two_factor? ? "true".colorize.green : "false".colorize.red)
-
-        stdout << "\n┃ ".colorize.light_gray << "created:   ".colorize.bold
-        user.created_at.to_s(stdout, "%F %R")
-
-        stdout << "\n┃ ".colorize.light_gray << "updated:   ".colorize.bold
-        if updated = user.updated_at
-          updated.to_s(stdout, "%F %R")
-        else
-          stdout << "N/A".colorize.dark_gray
-        end
+        user.to_s(stdout, width)
       end
     end
 
@@ -236,41 +167,7 @@ module Soar::Commands::App
 
         user = request post: "/api/application/users", data: input, as: Models::User
         width = 2 + (Math.log(user.id.to_f + 1) / Math.log(10)).ceil.to_i
-
-        Colorize.with.bold.on_light_gray.surround(stdout) do |io|
-          io << user.id.to_s.center(width)
-        end
-
-        if external = user.external_id
-          stdout << ' '
-          Colorize.with.dark_gray.surround(stdout) do |io|
-            io << '[' << external << ']'
-          end
-        end
-
-        stdout << "\n\n┃ ".colorize.light_gray << "username:  ".colorize.bold
-        stdout << user.username
-        stdout << "\n┃ ".colorize.light_gray << "full name: ".colorize.bold
-        stdout << user.first_name << ' ' << user.last_name
-
-        stdout << "\n┃ ".colorize.light_gray << "email:     ".colorize.bold << user.email
-        stdout << "\n┃ ".colorize.light_gray << "language:  ".colorize.bold << user.language
-
-        stdout << "\n┃ ".colorize.light_gray << "is admin:  ".colorize.bold
-        stdout << (user.root_admin? ? "true".colorize.green : "false".colorize.red)
-
-        stdout << "\n┃ ".colorize.light_gray << "has 2FA:   ".colorize.bold
-        stdout << (user.two_factor? ? "true".colorize.green : "false".colorize.red)
-
-        stdout << "\n┃ ".colorize.light_gray << "created:   ".colorize.bold
-        user.created_at.to_s(stdout, "%F %R")
-
-        stdout << "\n┃ ".colorize.light_gray << "updated:   ".colorize.bold
-        if updated = user.updated_at
-          updated.to_s(stdout, "%F %R")
-        else
-          stdout << "N/A".colorize.dark_gray
-        end
+        user.to_s(stdout, width)
       end
     end
 
--- src/models.cr
+++ src/models.cr
@@ -41,5 +41,42 @@ module Soar::Models
     property? two_factor : Bool
     getter created_at : Time
     getter updated_at : Time?
+
+    def to_s(io : IO, width : Int32) : Nil
+      Colorize.with.bold.on_light_gray.surround(io) do |_io|
+        _io << id.to_s.center(width)
+      end
+
+      if external = external_id
+        io << ' '
+        Colorize.with.dark_gray.surround(io) do |_io|
+          _io << '[' << external << ']'
+        end
+      end
+
+      io << "\n\n┃ ".colorize.light_gray << "username:  ".colorize.bold
+      io << username
+      io << "\n┃ ".colorize.light_gray << "full name: ".colorize.bold
+      io << first_name << ' ' << last_name
+
+      io << "\n┃ ".colorize.light_gray << "email:     ".colorize.bold << email
+      io << "\n┃ ".colorize.light_gray << "language:  ".colorize.bold << language
+
+      io << "\n┃ ".colorize.light_gray << "is admin:  ".colorize.bold
+      io << (root_admin? ? "true".colorize.green : "false".colorize.red)
+
+      io << "\n┃ ".colorize.light_gray << "has 2FA:   ".colorize.bold
+      io << (two_factor? ? "true".colorize.green : "false".colorize.red)
+
+      io << "\n┃ ".colorize.light_gray << "created:   ".colorize.bold
+      created_at.to_s(io, "%F %R")
+
+      io << "\n┃ ".colorize.light_gray << "updated:   ".colorize.bold
+      if updated = updated_at
+        updated.to_s(io, "%F %R")
+      else
+        io << "N/A".colorize.dark_gray
+      end
+    end
   end
 end
