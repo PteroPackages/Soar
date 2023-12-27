977b8a6e542490a5ce971ddf401c4105d4b69ea8
devnote-dev <devnote.dev75@gmail.com>
2023-12-27 14:10:10 +0000

    feat(app): implement users get subcommand

--- src/commands/app/users.cr
+++ src/commands/app/users.cr
@@ -4,6 +4,7 @@ module Soar::Commands::App
       @name = "users"
 
       add_command List.new
+      add_command Get.new
     end
 
     def run(arguments : Cling::Arguments, options : Cling::Options) : Nil
@@ -123,6 +124,67 @@ module Soar::Commands::App
           stdout << "no filters applied"
         else
           stdout << "filters: ".colorize.dark_gray << @filters.join(", ")
+        end
+      end
+    end
+
+    private class Get < Base
+      def setup : Nil
+        @name = "get"
+
+        add_argument "id"
+        add_option 'e', "external"
+        add_option "json"
+      end
+
+      def run(arguments : Cling::Arguments, options : Cling::Options) : Nil
+        arg = arguments.get("id").as_s
+
+        if options.has? "external"
+          user = request get: "/api/application/users/external/#{arg}", as: Models::User
+        else
+          user = request get: "/api/application/users/#{arg}", as: Models::User
+        end
+
+        if options.has? "json"
+          user.to_json stdout
+          return
+        end
+
+        width = 2 + (Math.log(user.id.to_f + 1) / Math.log(10)).ceil.to_i
+        Colorize.with.bold.on_light_gray.surround(stdout) do |io|
+          io << user.id.to_s.center(width)
+        end
+
+        if external = user.external_id
+          stdout << ' '
+          Colorize.with.dark_gray.surround(stdout) do |io|
+            io << '[' << external << ']'
+          end
+        end
+
+        stdout << "\n\n┃ ".colorize.light_gray << "username:  ".colorize.bold
+        stdout << user.username
+        stdout << "\n┃ ".colorize.light_gray << "full name: ".colorize.bold
+        stdout << user.first_name << ' ' << user.last_name
+
+        stdout << "\n┃ ".colorize.light_gray << "email:     ".colorize.bold << user.email
+        stdout << "\n┃ ".colorize.light_gray << "language:  ".colorize.bold << user.language
+
+        stdout << "\n┃ ".colorize.light_gray << "is admin:  ".colorize.bold
+        stdout << (user.root_admin? ? "true".colorize.green : "false".colorize.red)
+
+        stdout << "\n┃ ".colorize.light_gray << "has 2FA:   ".colorize.bold
+        stdout << (user.two_factor? ? "true".colorize.green : "false".colorize.red)
+
+        stdout << "\n┃ ".colorize.light_gray << "created:   ".colorize.bold
+        user.created_at.to_s(stdout, "%F %R")
+
+        stdout << "\n┃ ".colorize.light_gray << "updated:   ".colorize.bold
+        if updated = user.updated_at
+          updated.to_s(stdout, "%F %R")
+        else
+          stdout << "N/A".colorize.dark_gray
         end
       end
     end
