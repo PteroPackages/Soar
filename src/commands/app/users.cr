1f5d75935bc1f83ae5b94cb4a8274d9748c51770
devnote-dev <devnote.dev75@gmail.com>
2023-12-27 21:24:54 +0000

    feat(app): implement user create subcommand with input

--- src/commands/app/users.cr
+++ src/commands/app/users.cr
@@ -5,6 +5,7 @@ module Soar::Commands::App
 
       add_command List.new
       add_command Get.new
+      add_command Create.new
     end
 
     def run(arguments : Cling::Arguments, options : Cling::Options) : Nil
@@ -152,6 +153,89 @@ module Soar::Commands::App
         end
 
         width = 2 + (Math.log(user.id.to_f + 1) / Math.log(10)).ceil.to_i
+        Colorize.with.bold.on_light_gray.surround(stdout) do |io|
+          io << user.id.to_s.center(width)
+        end
+
+        if external = user.external_id
+          stdout << ' '
+          Colorize.with.dark_gray.surround(stdout) do |io|
+            io << '[' << external << ']'
+          end
+        end
+
+        stdout << "\n\n┃ ".colorize.light_gray << "username:  ".colorize.bold
+        stdout << user.username
+        stdout << "\n┃ ".colorize.light_gray << "full name: ".colorize.bold
+        stdout << user.first_name << ' ' << user.last_name
+
+        stdout << "\n┃ ".colorize.light_gray << "email:     ".colorize.bold << user.email
+        stdout << "\n┃ ".colorize.light_gray << "language:  ".colorize.bold << user.language
+
+        stdout << "\n┃ ".colorize.light_gray << "is admin:  ".colorize.bold
+        stdout << (user.root_admin? ? "true".colorize.green : "false".colorize.red)
+
+        stdout << "\n┃ ".colorize.light_gray << "has 2FA:   ".colorize.bold
+        stdout << (user.two_factor? ? "true".colorize.green : "false".colorize.red)
+
+        stdout << "\n┃ ".colorize.light_gray << "created:   ".colorize.bold
+        user.created_at.to_s(stdout, "%F %R")
+
+        stdout << "\n┃ ".colorize.light_gray << "updated:   ".colorize.bold
+        if updated = user.updated_at
+          updated.to_s(stdout, "%F %R")
+        else
+          stdout << "N/A".colorize.dark_gray
+        end
+      end
+    end
+
+    private class Create < Base
+      def setup : Nil
+        @name = "create"
+
+        add_option 'd', "data", type: :single
+        add_option 'i', "input"
+      end
+
+      def pre_run(arguments : Cling::Arguments, options : Cling::Options) : Bool
+        super
+        has_data = options.has? "data"
+        has_input = options.has? "input"
+
+        if has_data && has_input
+          error "cannot specify 'data' and 'input' flag; pick one"
+          return false
+        elsif !has_data && !has_input
+          error "either 'data' or 'input' option must be specified"
+          return false
+        end
+
+        true
+      end
+
+      def run(arguments : Cling::Arguments, options : Cling::Options) : Nil
+        if options.has? "input"
+          if stdin.closed?
+            error "cannot read from input file (already closed)"
+            return
+          end
+
+          input = Resolver.parse_json_or_map stdin.gets_to_end.chomp
+        else
+          input = Resolver.parse_json_or_map options.get("data").as_s
+        end
+
+        {"username", "email", "first_name", "last_name"}.each do |name|
+          unless input.has_key? name
+            error "Missing required key '#{name}' to create user"
+            return
+          end
+        end
+
+        user = request post: "/api/application/users", data: input, as: Models::User
+        width = 2 + (Math.log(user.id.to_f + 1) / Math.log(10)).ceil.to_i
+
         Colorize.with.bold.on_light_gray.surround(stdout) do |io|
           io << user.id.to_s.center(width)
         end
